a353e27f4b996926851f48a963e3bffc7cdf3a7e
Djet78 <vacheslavuslistyj@gmail.com>
2018-05-13 14:02:42 +0300

    replace few strings from function

--- homework_9.py
+++ homework_9.py
@@ -5,18 +5,16 @@ def snake_style_converter(phrase):
     """
     Func. takes given string, removes all '_' between words and saves in 'phrase'.
     First 'for' cycle takes words from  'phrase', makes first letters capitals and saves in 'phrase_cap'.
-    Second 'for' cycle takes words from  'phrase_cap' and adds up them in 'camel_case'
+    After it uses 'join' function and saves all phrase in 'camel_case'
     At last displays 'camel_case' phrase on the screen
     """
     try:
         phrase = phrase.split("_")
         phrase_cap = []
-        camel_case = ""
         for sequence in phrase:
             word_cap = sequence.capitalize()
             phrase_cap.append(word_cap)
-        for sequence in phrase_cap:
-            camel_case += sequence
+        camel_case = ''.join(phrase_cap)
         print(camel_case)
     except AttributeError:
         print("\n\tERROR:\n\tType of variable given to func. was wrong, it`s should be string")
@@ -24,3 +22,4 @@ def snake_style_converter(phrase):
 
 print("\nSnake style string: 'employee_first_name_and_second_name'\nTransforms into a Camel case string:")
 snake_style_converter("employee_first_name_and_second_name")
+snake_style_converter("another_example")
