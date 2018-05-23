53730d3da1a9861b39b2d72ea2c78fe8774824bf
Djet78 <vacheslavuslistyj@gmail.com>
2018-05-23 11:29:55 +0300

    I have reworked function like u asked, but first item '1' in list always stays at same place.

--- homework_25.py
+++ homework_25.py
@@ -4,17 +4,31 @@
 from random import randint
 
 
-def shuffle_list(list_to_shuffle):
+# def shuffle_list(list_to_shuffle):
+#     """
+#     Shuffles all data in given list between each other with creating new list
+#     :param list_to_shuffle: takes given list
+#     :return: None
+#     """
+#     shuffled_list = []
+#     for i in list_to_shuffle:
+#         shuffled_list.insert(randint(1, 100), i)
+#     print(shuffled_list)
+#
+#
+lst = range(1, 100)[::2]
+# shuffle_list(lst)
+
+
+def shuffle_list2(list_to_shuffle):
     """
-    Shuffles all data in given list between each other
+    Shuffles all data in given list between each other w/o creating new list
     :param list_to_shuffle: takes given list
     :return: None
     """
-    shuffled_list = []
-    for i in list_to_shuffle:
-        shuffled_list.insert(randint(1, 100), i)
-    print(shuffled_list)
+    for _ in list_to_shuffle:
+        list_to_shuffle.insert(randint(1, 20), list_to_shuffle.pop())
+    print(list_to_shuffle)
 
 
-lst = range(1, 100)[::2]
-shuffle_list(lst)
+shuffle_list2(list(lst))
