96589e3a75f004e015584ea48e68d4e4c5f5fe1b
Djet78 <vacheslavuslistyj@gmail.com>
2018-05-25 19:14:31 +0300

    Have changed s.insert(s.pop) to s.append(s.pop)

--- homework_25.py
+++ homework_25.py
@@ -1,8 +1,6 @@
 # ------------------------ Task 25 -------------------------
 
-
 from random import randint
-
 
 # def shuffle_list(list_to_shuffle):
 #     """
@@ -16,19 +14,27 @@ from random import randint
 #     print(shuffled_list)
 #
 #
-lst = range(1, 100)[::2]
-# shuffle_list(lst)
+# def shuffle_list2(list_to_shuffle):
+#     """
+#     Shuffles all data in given list between each other w/o creating new list
+#     :param list_to_shuffle: takes given list
+#     :return: None
+#     """
+#     for _ in list_to_shuffle:
+#         list_to_shuffle.insert(randint(1, 20), list_to_shuffle.pop())
+#     print(list_to_shuffle)
 
 
-def shuffle_list2(list_to_shuffle):
+def shuffle_list3(list_to_shuffle):
     """
     Shuffles all data in given list between each other w/o creating new list
     :param list_to_shuffle: takes given list
     :return: None
     """
     for _ in list_to_shuffle:
-        list_to_shuffle.insert(randint(1, 20), list_to_shuffle.pop())
+        list_to_shuffle.append(list_to_shuffle.pop(randint(1, 10)))
     print(list_to_shuffle)
 
 
-shuffle_list2(list(lst))
+lst = list(range(1, 100)[::2])
+shuffle_list3(lst)
