ccd2262d5fed02ebaee2b6ef18aa573220cafe30
Djet78 <vacheslavuslistyj@gmail.com>
2018-06-04 19:21:49 +0300

    Made from both variants functions. Used while loop in second function

--- test_4.py
+++ test_4.py
@@ -1,25 +1,45 @@
 # ----------------------- task 4 ------------------------
 # First variant
 
-num = input("Enter your number, and program count all not even digits in it: ")
-total = 0
-for digit in num:
-        if not int(digit) % 2 == 0:
-            total += int(digit)
-print(total)
 
+def not_even_digits_multiply_1():
+    """
+    Multiply all not even digits in number
+    :return: Int. multiplied value
+    """
+    try:
+        num = input("Enter your number, and program multiply all not even digits in it: ")
+        total = 1
+        for digit in num:
+                if not int(digit) % 2 == 0:
+                    total *= int(digit)
+        return total
+    except Exception as ex:
+        print("Error!! You have entered non digit symbol: ", ex)
+
+
+print(not_even_digits_multiply_1())
 # Second variant
-num_2 = int(input("Enter your five digit number, and program count all not even digits in it: "))
-total_2 = 0
-if 1e4 <= num_2 <= 1e5 - 1:
-    lst_of_digits = []
-    lst_of_digits.append(num_2 // 10000)
-    lst_of_digits.append((num_2 // 1000) % 10)
-    lst_of_digits.append((num_2 // 100) % 10)
-    lst_of_digits.append((num_2 // 10) % 10)
-    lst_of_digits.append(num_2 % 10)
-    for digit in lst_of_digits:
-        if not digit % 2 == 0:
-            total_2 += digit
 
-print(total_2)
+
+def not_even_digits_multiply_2():
+    """
+    Multiply all not even digits in number
+    :return: Int. multiplied value
+    """
+    try:
+        num = int(input("Enter your number, and program multiply all not even digits in it: "))
+        total = 1
+        lst_of_digits = []
+        while num != 0:
+            lst_of_digits.append(num % 10)
+            num = num // 10
+        for digit in lst_of_digits:
+            if not digit % 2 == 0:
+                total *= digit
+        return total
+    except Exception as ex:
+        print("Error!! You have entered non digit symbol: ", ex)
+
+
+print(not_even_digits_multiply_2())
