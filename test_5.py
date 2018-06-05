f8ac7499d551c5114d1da535b4ec93481050a59b
Djet78 <vacheslavuslistyj@gmail.com>
2018-06-05 14:54:47 +0300

    Removed excess strings of code

--- test_5.py
+++ test_5.py
@@ -7,20 +7,8 @@ def nearest_to_ten(first_num, second_num):
     :param second_num:
     :return: nearest number to ten
     """
-    if first_num < 10:
-        if first_num <= 0:
-            first_distance = abs(first_num) + 10
-        else:
-            first_distance = 10 - first_num
-    else:
-        first_distance = first_num - 10
-    if second_num < 10:
-        if second_num <= 0:
-            second_distance = abs(second_num) + 10
-        else:
-            second_distance = 10 - second_num
-    else:
-        second_distance = second_num - 10
+    first_distance = abs(first_num - 10)
+    second_distance = abs(second_num - 10)
     if first_distance > second_distance:
         return second_num
     else:
