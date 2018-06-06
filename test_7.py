a2cb2e2a14df9e4c460542f3629dcc0926fec482
Djet78 <vacheslavuslistyj@gmail.com>
2018-06-07 01:05:17 +0300

    Generalized code

--- test_7.py
+++ test_7.py
@@ -14,24 +14,18 @@ def fibonacci_calculator(n):
             return 1
         elif n == -1:
             return -1
-        elif n < -1:
-            total_num = 0
-            back_num1 = -1
-            back_num2 = 0
-            for num in range(n, -1):
-                total_num = back_num1 + back_num2
-                back_num2 = back_num1
-                back_num1 = total_num
-            return total_num
-        elif n > 1:
+        elif abs(n) > 1:
             total_num = 0
             back_num1 = 1
             back_num2 = 0
-            for num in range(1, n):
+            for num in range(1, abs(n)):
                 total_num = back_num1 + back_num2
                 back_num2 = back_num1
                 back_num1 = total_num
-            return total_num
+            if n < -1:
+                return -total_num
+            else:
+                return total_num
     except Exception as ex:
         print("Error occurred!!!", ex)
 
