5a61c4c2c92773096135923e7eae3fc4af475606
Djet78 <vacheslavuslistyj@gmail.com>
2018-06-07 00:51:29 +0300

    Used received knowledge from last lesson

--- homework_25.py
+++ homework_25.py
@@ -2,43 +2,8 @@
 
 from random import randint
 
-# def shuffle_list(list_to_shuffle):
-#     """
-#     Shuffles all data in given list between each other with creating new list
-#     :param list_to_shuffle: takes given list
-#     :return: None
-#     """
-#     shuffled_list = []
-#     for i in list_to_shuffle:
-#         shuffled_list.insert(randint(1, 100), i)
-#     print(shuffled_list)
-#
-#
-# def shuffle_list2(list_to_shuffle):
-#     """
-#     Shuffles all data in given list between each other w/o creating new list
-#     :param list_to_shuffle: takes given list
-#     :return: None
-#     """
-#     for _ in list_to_shuffle:
-#         list_to_shuffle.append(list_to_shuffle.pop(randint(1, 10)))
-#     print(list_to_shuffle)
 
-
-def shuffle_list3(list_to_shuffle):
-    """
-    Shuffles all data in given list between each other w/o creating new list.
-    :param list_to_shuffle: takes given list
-    :return: None
-    """
-    i = 0
-    while i < 50:
-        rand_iter = randint(0, len(s) - 1)
-        s.append(s[rand_iter])
-        s.remove(s[rand_iter])
-        i += 1
-    print(list_to_shuffle)
-
-
-s = list(range(1, 100)[::2])
-shuffle_list3(s)
+not_even_list = [i for i in range(1, 100, 2)]
+print(not_even_list)
+not_even_list.sort(key=lambda elem: elem + randint(1, 100))
+print(not_even_list)
