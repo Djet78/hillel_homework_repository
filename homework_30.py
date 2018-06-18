57d423cd46391b969f9ffa111debebd15e716836
Djet78 <vacheslavuslistyj@gmail.com>
2018-06-18 17:34:59 +0300

    h/w 30

---
+++ homework_30.py
@@ -0,0 +1,26 @@
+# ------------------------ Task 30 -------------------------
+
+
+def gen_primes(n):
+    """
+    Generate primes in given range 'n'
+    :param n: define range
+    :return: list. All prime numbers on given range 'n'
+    """
+    lst = [2]
+    for i in range(3, n + 1, 2):
+        if i > 10:
+            if (i % 2 == 0) or (i % 10 == 5):
+                continue
+        for j in lst:
+            if j * j - 1 > i:
+                lst.append(i)
+                break
+            if i % j == 0:
+                break
+            else:
+                lst.append(i)
+    return lst
+
+
+print(gen_primes(10000))
