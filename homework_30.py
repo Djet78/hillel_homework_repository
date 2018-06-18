7ae2d04b4b3b5d91e44834641faf4c3111581588
Djet78 <vacheslavuslistyj@gmail.com>
2018-06-18 23:02:56 +0300

    У меня таки да, была ошибка в коде, но функция фозвращает правильный результат, если верить вики, не знаю чего бот ругается. Выдает: 'name 'gen_primes' is not defined'

--- homework_30.py
+++ homework_30.py
@@ -9,18 +9,17 @@ def gen_primes(n):
     """
     lst = [2]
     for i in range(3, n + 1, 2):
-        if i > 10:
-            if (i % 2 == 0) or (i % 10 == 5):
-                continue
+        if (i > 10) and (i % 10 == 5):
+            continue
         for j in lst:
             if j * j - 1 > i:
                 lst.append(i)
                 break
             if i % j == 0:
                 break
-            else:
-                lst.append(i)
+        else:
+            lst.append(i)
     return lst
 
 
-print(gen_primes(10000))
+print(gen_primes(150))
